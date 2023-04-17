17c4ce5b327c32c6b7a4c608e35a8cac6bc06b52
ShoroukAziz <shorouk.s.abdelaziz@gmail.com>
2023-04-17 01:05:04 -0600

    topics seeds

---
+++ prisma/seeds/data/topics.js
@@ -0,0 +1,74 @@
+module.exports = [
+  {
+    id: '8e3399e6-1d94-11ec-9621-0242ac130002',
+    userId: 'f1bdf45e-1b1c-11ec-9621-0242ac130002',
+    name: 'JavaScript',
+    created_at: new Date(
+      'Tue Sep 21 2021 16:16:50 GMT-0400 (Mountain Daylight Time)'
+    ),
+    updated_at: new Date(
+      'Tue Sep 21 2021 16:16:50 GMT-0400 (Mountain Daylight Time)'
+    ),
+    max_cards: 25,
+  },
+  {
+    id: '92e934f4-dce8-11ed-afa1-0242ac120002',
+    userId: 'f1bdf45e-1b1c-11ec-9621-0242ac130002',
+    name: 'React',
+    created_at: new Date(
+      'Tue Sep 25 2021 16:16:50 GMT-0400 (Mountain Daylight Time)'
+    ),
+    updated_at: new Date(
+      'Tue Sep 25 2021 16:16:50 GMT-0400 (Mountain Daylight Time)'
+    ),
+    max_cards: 25,
+  },
+  {
+    id: 'b09a169e-dce8-11ed-afa1-0242ac120002',
+    userId: 'f1bdf45e-1b1c-11ec-9621-0242ac130002',
+    name: 'Ruby',
+    created_at: new Date(
+      'Tue Sep 24 2021 16:16:50 GMT-0400 (Mountain Daylight Time)'
+    ),
+    updated_at: new Date(
+      'Tue Sep 24 2021 16:16:50 GMT-0400 (Mountain Daylight Time)'
+    ),
+    max_cards: 25,
+  },
+  {
+    id: 'c4644dde-dce8-11ed-afa1-0242ac120002',
+    userId: 'f1bdf45e-1b1c-11ec-9621-0242ac130002',
+    name: 'SQL',
+    created_at: new Date(
+      'Tue Sep 24 2021 16:16:50 GMT-0400 (Mountain Daylight Time)'
+    ),
+    updated_at: new Date(
+      'Tue Sep 24 2021 16:16:50 GMT-0400 (Mountain Daylight Time)'
+    ),
+    max_cards: 25,
+  },
+  {
+    id: '7dcdcaf2-dce9-11ed-afa1-0242ac120002',
+    userId: 'f1bdf45e-1b1c-11ec-9621-0242ac130002',
+    name: 'CSS',
+    created_at: new Date(
+      'Tue Sep 24 2021 16:16:50 GMT-0400 (Mountain Daylight Time)'
+    ),
+    updated_at: new Date(
+      'Tue Sep 24 2021 16:16:50 GMT-0400 (Mountain Daylight Time)'
+    ),
+    max_cards: 25,
+  },
+  {
+    id: 'c5f69d40-dce9-11ed-afa1-0242ac120002',
+    userId: 'f1bdf45e-1b1c-11ec-9621-0242ac130002',
+    name: 'HTML',
+    created_at: new Date(
+      'Tue Sep 24 2021 16:16:50 GMT-0400 (Mountain Daylight Time)'
+    ),
+    updated_at: new Date(
+      'Tue Sep 24 2021 16:16:50 GMT-0400 (Mountain Daylight Time)'
+    ),
+    max_cards: 25,
+  },
+];
