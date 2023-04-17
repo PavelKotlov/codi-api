1ace0aa69b12c54cd035b5a7ffadbf3dbfa6b318
ShoroukAziz <shorouk.s.abdelaziz@gmail.com>
2023-04-17 01:07:34 -0600

    ➕ example of using prisma

--- routes/topics.js
+++ routes/topics.js
@@ -1,4 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+//TODO: Delete me - Example of using Prisma
+router.get('/', (req, res) => {
+  prisma.topic.findMany().then((data) => {
+    res.json(data);
+  });
+});
 
 module.exports = router;
