e7adf6b5f92b77782aab830d4c24dd1af45836a9
PavelKotlov <pavelkotlov@gmail.com>
2023-04-16 18:59:33 -0700

    Setup

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
-const Express = require('express');
-const App = Express();
-const BodyParser = require('body-parser');
-const PORT = 8080;
+const express = require('express');
+const App = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const methodOverride = require('method-override');
+const PORT = 3000;
 
-// Express Configuration
-App.use(BodyParser.urlencoded({ extended: false }));
-App.use(BodyParser.json());
-App.use(Express.static('public'));
+// express Configuration
+App.use(morgan('dev'));
+App.use(bodyParser.urlencoded({ extended: false }));
+App.use(bodyParser.json());
+
+App.use(express.static('public'));
+App.use(methodOverride('_method'))
+
+
+const topicsRoutes = require('./routes/topics');
+const cardsRoutes = require('./routes/cards');
+const usersRoutes = require('./routes/users');
+const loginRoutes = require('./routes/login');
+const logoutRoutes = require('./routes/logout');
+const signupRoutes = require('./routes/signup');
+
+app.use('/topics', topicsRoutes);
+app.use('/topics/:id/cards', cardsRoutes);
+app.use('/users', usersRoutes);
+app.use('/login', loginRoutes);
+app.use('/logout', logoutRoutes);
+app.use('/signup', signupRoutes);
 
 // Sample GET route
 App.get('/api/data', (req, res) => res.json({
@@ -15,5 +35,5 @@ App.get('/api/data', (req, res) => res.json({
 
 App.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log(`Express seems to be listening on port ${PORT} so that's pretty good 👍`);
+  console.log(`express seems to be listening on port ${PORT} so that's pretty good 👍`);
 });
