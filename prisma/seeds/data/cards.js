ea718b9e320b5626585b97e945bb1a33584cbe2b
ShoroukAziz <shorouk.s.abdelaziz@gmail.com>
2023-04-17 01:04:46 -0600

    ➕ cards seeds

---
+++ prisma/seeds/data/cards.js
@@ -0,0 +1,101 @@
+module.exports = [
+  {
+    id: '63523fc0-dce2-11ed-afa1-0242ac120002',
+    topicId: '8e3399e6-1d94-11ec-9621-0242ac130002',
+    front: 'What is a callback function in javascript?',
+    back: 'In JavaScript, a callback function is a function that is passed as an argument to another function and is executed when that function finishes its task.',
+    status: 'NEW',
+    type: 'CONCEPT',
+    created_at: new Date(
+      'Tue Sep 22 2021 16:16:50 GMT-0400 (Mountain Daylight Time)'
+    ),
+    updated_at: new Date(
+      'Tue Sep 22 2021 16:16:50 GMT-0400 (Mountain Daylight Time)'
+    ),
+    tags: {
+      connect: {
+        name: 'functions',
+      },
+    },
+  },
+  {
+    id: '56410106-dcea-11ed-afa1-0242ac120002',
+    topicId: 'b09a169e-dce8-11ed-afa1-0242ac120002',
+    front: 'What is a module in Ruby?',
+    back: 'A module in Ruby is a collection of related methods, classes, and constants that can be used to share functionality among multiple classes or to organize code within a single class.',
+    status: 'LEARNING',
+    type: 'CONCEPT',
+    created_at: new Date(
+      'Tue Sep 22 2021 16:16:50 GMT-0400 (Mountain Daylight Time)'
+    ),
+    updated_at: new Date(
+      'Tue Sep 22 2021 16:16:50 GMT-0400 (Mountain Daylight Time)'
+    ),
+    tags: {
+      connect: [
+        {
+          name: 'oop',
+        },
+        {
+          name: 'modules',
+        },
+      ],
+    },
+    reviews: {
+      create: {
+        reponse_type: 'GOOD',
+        reviewd_at: new Date(
+          'Tue Sep 22 2021 18:16:50 GMT-0400 (Mountain Daylight Time)'
+        ),
+      },
+    },
+  },
+  {
+    id: '28c59b6e-dceb-11ed-afa1-0242ac120002',
+    topicId: 'b09a169e-dce8-11ed-afa1-0242ac120002',
+    front: 'What is one of the main benefits of using modules in Ruby?',
+    back: " One of the main benefits of using modules in Ruby is that they provide a way to achieve multiple inheritance without the need for a class to inherit from the module's superclass.",
+    status: 'GRADUATED',
+    type: 'CONCEPT',
+    created_at: new Date(
+      'Tue Sep 22 2021 16:16:50 GMT-0400 (Mountain Daylight Time)'
+    ),
+    updated_at: new Date(
+      'Tue Sep 22 2021 16:16:50 GMT-0400 (Mountain Daylight Time)'
+    ),
+    tags: {
+      connect: [
+        {
+          name: 'modules',
+        },
+        {
+          name: 'oop',
+        },
+        {
+          name: 'inheritance',
+        },
+      ],
+    },
+    reviews: {
+      create: [
+        {
+          reponse_type: 'GOOD',
+          reviewd_at: new Date(
+            'Tue Sep 22 2021 18:16:50 GMT-0400 (Mountain Daylight Time)'
+          ),
+        },
+        {
+          reponse_type: 'GOOD',
+          reviewd_at: new Date(
+            'Tue Sep 22 2021 18:36:50 GMT-0400 (Mountain Daylight Time)'
+          ),
+        },
+      ],
+    },
+    ease_factor: 2.5,
+    interval: 1,
+    due_at: new Date(
+      'Tue Sep 23 2021 18:36:50 GMT-0400 (Mountain Daylight Time)'
+    ),
+  },
+];
