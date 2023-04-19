14d9c8c8968a074537f26afdf1867ed8ec3435f7
ShoroukAziz <shorouk.s.abdelaziz@gmail.com>
2023-04-19 15:59:41 -0600

    dummy reviews for heat map

---
+++ prisma/seeds/data/cards/dummyReviewsForHeatMap.js
@@ -0,0 +1,24 @@
+const { daysAgo } = require('../../helper');
+
+const getRandomInt = function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+};
+
+const createDummyReviewsForHeatMap = function () {
+  const dummyReviews = [];
+  const days = [...Array(250).keys()];
+  days.forEach((day) => {
+    const reviewsForDay = [...Array(getRandomInt(26)).keys()];
+    reviewsForDay.forEach((e) => {
+      const review = {
+        reponse_type: 'GOOD',
+        reviewd_at: daysAgo(day),
+        cardId: '2da8814a-def9-11ed-b5ea-0242ac120002',
+      };
+      dummyReviews.push(review);
+    });
+  });
+  return dummyReviews;
+};
+
+module.exports = createDummyReviewsForHeatMap();
