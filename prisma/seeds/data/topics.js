5e3bb683f121ba329b5a5b31cc7cffcb43cde3f4
ShoroukAziz <shorouk.s.abdelaziz@gmail.com>
2023-04-21 12:54:06 -0600

    rename topics

--- prisma/seeds/data/topics.js
+++ prisma/seeds/data/topics.js
@@ -11,6 +11,7 @@ module.exports = [
     created_at,
     updated_at,
     userId,
+    image_url: 'https://i.ibb.co/4PqwXFr/icon-02.png',
   },
   {
     id: '92e934f4-dce8-11ed-afa1-0242ac120002',
@@ -19,6 +20,7 @@ module.exports = [
     created_at,
     updated_at,
     userId,
+    image_url: 'https://i.ibb.co/QCR3t7c/icon-06.png',
   },
   {
     id: 'b09a169e-dce8-11ed-afa1-0242ac120002',
@@ -27,14 +29,16 @@ module.exports = [
     created_at,
     updated_at,
     userId,
+    image_url: 'https://i.ibb.co/tHYVjKY/icon-05.png',
   },
   {
     id: 'c4644dde-dce8-11ed-afa1-0242ac120002',
-    name: 'Databases & SQL',
+    name: 'Databases',
     max_cards,
     created_at,
     updated_at,
     userId,
+    image_url: 'https://i.ibb.co/b5nV4TZ/icon-07.png',
   },
   {
     id: '7dcdcaf2-dce9-11ed-afa1-0242ac120002',
@@ -43,6 +47,7 @@ module.exports = [
     created_at,
     updated_at,
     userId,
+    image_url: 'https://i.ibb.co/B4ZrvXM/icon-04.png',
   },
   {
     id: 'c5f69d40-dce9-11ed-afa1-0242ac120002',
@@ -51,13 +56,15 @@ module.exports = [
     created_at,
     updated_at,
     userId,
+    image_url: 'https://i.ibb.co/fQDc5YD/icon-03.png',
   },
   {
     id: '5760c81c-dd73-11ed-b5ea-0242ac120002',
-    name: 'Data Structures & Algorithms',
+    name: 'DSA',
     max_cards,
     created_at,
     updated_at,
     userId,
+    image_url: 'https://i.ibb.co/18CQnVm/icon-08.png',
   },
 ];
