d70e868eb47aa70882802be0cc9391ceba3c78d5
ShoroukAziz <shorouk.s.abdelaziz@gmail.com>
2023-04-23 20:57:06 -0600

    change seeds with new user id

--- prisma/seeds/data/topics.js
+++ prisma/seeds/data/topics.js
@@ -1,71 +1,71 @@
-const created_at = new Date("Sep 21 2021 16:16:50 GMT-0400");
-const created_at_js = new Date("Sep 21 2021 18:16:50 GMT-0400");
-const updated_at = new Date("Sep 21 2021 16:16:50 GMT-0400");
-const userId = "f1bdf45e-1b1c-11ec-9621-0242ac130002";
+const created_at = new Date('Sep 21 2021 16:16:50 GMT-0400');
+const created_at_js = new Date('Sep 21 2021 18:16:50 GMT-0400');
+const updated_at = new Date('Sep 21 2021 16:16:50 GMT-0400');
+const userId = 'google-oauth2|104642415547171430637';
 const max_cards = 25;
 
 module.exports = [
   {
-    id: "8e3399e6-1d94-11ec-9621-0242ac130002",
-    name: "JavaScript",
+    id: '8e3399e6-1d94-11ec-9621-0242ac130002',
+    name: 'JavaScript',
     max_cards: 3,
     created_at: created_at_js,
     updated_at,
     userId,
-    image_url: "https://i.ibb.co/4PqwXFr/icon-02.png",
+    image_url: 'https://i.ibb.co/4PqwXFr/icon-02.png',
   },
   {
-    id: "92e934f4-dce8-11ed-afa1-0242ac120002",
-    name: "React",
+    id: '92e934f4-dce8-11ed-afa1-0242ac120002',
+    name: 'React',
     max_cards,
     created_at,
     updated_at,
     userId,
-    image_url: "https://i.ibb.co/QCR3t7c/icon-06.png",
+    image_url: 'https://i.ibb.co/QCR3t7c/icon-06.png',
   },
   {
-    id: "b09a169e-dce8-11ed-afa1-0242ac120002",
-    name: "Ruby",
+    id: 'b09a169e-dce8-11ed-afa1-0242ac120002',
+    name: 'Ruby',
     max_cards: 25,
     created_at,
     updated_at,
     userId,
-    image_url: "https://i.ibb.co/tHYVjKY/icon-05.png",
+    image_url: 'https://i.ibb.co/tHYVjKY/icon-05.png',
   },
   {
-    id: "c4644dde-dce8-11ed-afa1-0242ac120002",
-    name: "Databases",
+    id: 'c4644dde-dce8-11ed-afa1-0242ac120002',
+    name: 'Databases',
     max_cards,
     created_at,
     updated_at,
     userId,
-    image_url: "https://i.ibb.co/b5nV4TZ/icon-07.png",
+    image_url: 'https://i.ibb.co/b5nV4TZ/icon-07.png',
   },
   {
-    id: "7dcdcaf2-dce9-11ed-afa1-0242ac120002",
-    name: "CSS",
+    id: '7dcdcaf2-dce9-11ed-afa1-0242ac120002',
+    name: 'CSS',
     max_cards,
     created_at,
     updated_at,
     userId,
-    image_url: "https://i.ibb.co/B4ZrvXM/icon-04.png",
+    image_url: 'https://i.ibb.co/B4ZrvXM/icon-04.png',
   },
   {
-    id: "c5f69d40-dce9-11ed-afa1-0242ac120002",
-    name: "HTML",
+    id: 'c5f69d40-dce9-11ed-afa1-0242ac120002',
+    name: 'HTML',
     max_cards: 5,
     created_at,
     updated_at,
     userId,
-    image_url: "https://i.ibb.co/fQDc5YD/icon-03.png",
+    image_url: 'https://i.ibb.co/fQDc5YD/icon-03.png',
   },
   {
-    id: "5760c81c-dd73-11ed-b5ea-0242ac120002",
-    name: "DSA",
+    id: '5760c81c-dd73-11ed-b5ea-0242ac120002',
+    name: 'DSA',
     max_cards,
     created_at,
     updated_at,
     userId,
-    image_url: "https://i.ibb.co/18CQnVm/icon-08.png",
+    image_url: 'https://i.ibb.co/18CQnVm/icon-08.png',
   },
 ];
--- prisma/seeds/data/users.js
+++ prisma/seeds/data/users.js
@@ -1,10 +1,5 @@
 module.exports = [
   {
-    id: 'f1bdf45e-1b1c-11ec-9621-0242ac130002',
-    email: 'codi.app.x@gmail.com',
-  },
-  {
-    id: 'd25885fc-dd4e-11ed-b5ea-0242ac120002',
-    email: 'choicemateapp@gmail.com',
+    id: 'google-oauth2|104642415547171430637',
   },
 ];
