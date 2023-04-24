a8e3487dacea41ea9ae336c478504e1f5a6a2810
ShoroukAziz <shorouk.s.abdelaziz@gmail.com>
2023-04-23 20:57:30 -0600

    delete unused routers

--- routes/login.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', async (req, res) => {
-  //TODO: Gotta make sure this is the corret redirect
-  res.redirect('/codi-app.us.auth0.com');
-})
-
-module.exports = router;
