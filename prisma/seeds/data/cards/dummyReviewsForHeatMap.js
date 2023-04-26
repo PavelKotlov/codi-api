802b6a4c820864854f91d7659a4bfa315eaad272
PavelKotlov <pavelkotlov@gmail.com>
2023-04-26 11:54:31 -0700

    Refactor test data

--- prisma/seeds/data/cards/dummyReviewsForHeatMap.js
+++ prisma/seeds/data/cards/dummyReviewsForHeatMap.js
@@ -1,4 +1,4 @@
-const { daysAgo } = require('../../helper');
+const { daysAgo } = require("../../helper");
 
 const getRandomInt = function getRandomInt(max) {
   return Math.floor(Math.random() * max);
@@ -6,14 +6,14 @@ const getRandomInt = function getRandomInt(max) {
 
 const createDummyReviewsForHeatMap = function () {
   const dummyReviews = [];
-  const days = [...Array(250).keys()];
+  const days = [...Array(150).keys()];
   days.forEach((day) => {
     const reviewsForDay = [...Array(getRandomInt(26)).keys()];
     reviewsForDay.forEach((e) => {
       const review = {
-        reponse_type: 'GOOD',
+        reponse_type: "GOOD",
         reviewd_at: daysAgo(day),
-        cardId: '2da8814a-def9-11ed-b5ea-0242ac120002',
+        cardId: "2da8814a-def9-11ed-b5ea-0242ac120002",
       };
       dummyReviews.push(review);
     });
